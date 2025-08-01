7628c12406e529daf91b27a2d3627c239d2d9ab8
Dohyeop Lim <dhlim0817@gmail.com>
2025-08-01 18:18:08 +0900

    initialize project

---
+++ WONNIT/WONNITApp.swift
@@ -0,0 +1,17 @@
+//
+//  WONNITApp.swift
+//  WONNIT
+//
+//  Created by dohyeoplim on 8/1/25.
+//
+
+import SwiftUI
+
+@main
+struct WONNITApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
