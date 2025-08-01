e5fd88d3d8924b0aabd4746697e860f9ce5175b5
Dohyeop Lim <dhlim0817@gmail.com>
2025-08-01 22:29:30 +0900

    chore: compose project structures

--- WONNIT/App/WONNITApp.swift
+++ WONNIT/App/WONNITApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct WONNITApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            HomeView()
         }
     }
 }
---
+++ WONNIT/Features/Home/Views/HomeView.swift
@@ -0,0 +1,18 @@
+//
+//  HomeView.swift
+//  WONNIT
+//
+//  Created by dohyeoplim on 8/1/25.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HomeView()
+}
