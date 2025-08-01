5cac7ed184a40bab3792553b2a427f76e7830e74
Dohyeop Lim <dhlim0817@gmail.com>
2025-08-01 23:09:22 +0900

    feat: testing custom font application

---
+++ WONNIT/Design/Typography/Font+Extension.swift
@@ -0,0 +1,12 @@
+//
+//  Font+Extension.swift
+//  WONNIT
+//
+//  Created by dohyeoplim on 8/1/25.
+//
+
+import SwiftUI
+
+extension Font {
+    static let testFont: Font = .custom("PretendardVariable-Regular", size: 16)
+}
--- WONNIT/Features/Home/Views/HomeView.swift
+++ WONNIT/Features/Home/Views/HomeView.swift
@@ -9,7 +9,8 @@ import SwiftUI
 
 struct HomeView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Text("프리텐다드 Regular 16px")
+            .font(.testFont)
     }
 }
 
