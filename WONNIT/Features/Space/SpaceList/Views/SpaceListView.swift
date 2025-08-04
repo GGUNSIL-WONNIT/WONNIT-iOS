59914020b19c6e8958b908f75c6a87f250c4f49a
Dohyeop Lim <dhlim0817@gmail.com>
2025-08-05 02:37:39 +0900

    feat: add space list view

--- WONNIT/Features/Space/SpaceList/Views/SpaceListView.swift
+++ WONNIT/Features/Space/SpaceList/Views/SpaceListView.swift
@@ -8,18 +8,46 @@
 import SwiftUI
 
 struct SpaceListView: View {
+    @Environment(\.dismiss) private var dismiss
+    
     let category: SpaceCategory?
     
     var body: some View {
         ScrollView {
-            VStack {
-                Text("얍")
+            VStack(alignment: .leading, spacing: 20) {
+                Text("검색 결과 15개")
+                    .body_04(.grey700)
+                
+                ForEach(0..<15) { _ in
+                    SpacePreviewCardView(space: .placeholder, layout: .horizontal)
+                }
+                
+//                NotFoundView()
+//                    .frame(maxWidth: .infinity)
+            }
+            .padding(.horizontal)
+            .frame(maxWidth: .infinity, alignment: .leading)
+            .navigationTitle(category?.label ?? "최근 등록 장소")
+            .navigationBarBackButtonHidden()
+            .navigationBarTitleDisplayMode(.large)
+            .toolbarBackground(Color.white)
+            .toolbar {
+                ToolbarItem(placement: .topBarLeading) {
+                    Button {
+                        dismiss()
+                    } label: {
+                        Image(systemName: "chevron.left")
+                    }
+                    .foregroundStyle(Color.grey900)
+                    .font(.system(size: 18))
+                }
             }
         }
-        .navigationTitle(category?.label ?? "최근 등록 장소")
     }
 }
 
 #Preview {
-    SpaceListView(category: .makerSpace)
+    NavigationStack {
+        SpaceListView(category: .smallTheater)
+    }
 }
