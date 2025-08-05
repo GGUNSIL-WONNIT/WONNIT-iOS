4eeb905ae9df5c4a767787495c4a1065e980b27e
Dohyeop Lim <dhlim0817@gmail.com>
2025-08-05 11:55:18 +0900

    feat: add UIGestureRecognizer globally

---
+++ WONNIT/Shared/UI/Gestures/UINavigationController+Extensions.swift
@@ -0,0 +1,20 @@
+//
+//  UINavigationController+Extensions.swift
+//  WONNIT
+//
+//  Created by dohyeoplim on 8/5/25.
+//
+
+import Foundation
+import SwiftUI
+
+extension UINavigationController: @retroactive UIGestureRecognizerDelegate {
+    override open func viewDidLoad() {
+        super.viewDidLoad()
+        interactivePopGestureRecognizer?.delegate = self
+    }
+
+    public func gestureRecognizerShouldBegin(_ gestureRecognizer: UIGestureRecognizer) -> Bool {
+        return viewControllers.count > 1
+    }
+}
